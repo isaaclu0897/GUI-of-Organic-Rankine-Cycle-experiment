0ab2891675b7cb510bb64fa5ffd0b3ee1d4f9263
wei <t104306033@ntut.org.tw>
2018-11-19 15:29:57 +0800

    /datasave: clear

--- GUIObj.py
+++ GUIObj.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Mon Jul  9 00:24:49 2018
+Created on Mon Nov 19 15:01:08 2018
 
 @author: wei
 """
@@ -286,7 +286,7 @@ class ORC_Figure(tk.Frame):
             self.canvas.draw()
         
 
-def scan_data():
+def scan_data(on_click_loop, SM_dia, TH_dia):
 # =============================================================================
 # load the data
 # =============================================================================
@@ -303,7 +303,7 @@ def scan_data():
     data = SendData()
         
 
-    def innerfunc():
+    def innerfunc(on_click_loop, SM_dia, TH_dia):
         # scan temperature
         scans_TEMP = v34972A.query(':MEASure:TEMPerature? %s,%s,(%s)' % (probe_type_TEMP, type_TEMP, ch_TEMP))
         
@@ -322,23 +322,23 @@ def scan_data():
         readings_TEMP = [22, 25, 97, 64, 24, 68, 99, 89, 22, 24]
         
         data.send(readings_TEMP, readings_PRESS)
-        data.update()
+        data.update(SM_dia, TH_dia)
 
-    timer(innerfunc, 3,)
+    timer(innerfunc, 3, on_click_loop, SM_dia, TH_dia)
 
-def test_scan_data():
-    global data
+def test_scan_data(SM_dia, TH_dia):
+
     data = SendData()
 
-    def innerfunc():
+    def innerfunc(SM_dia, TH_dia):
         readings_PRESS = [1.8, 9, 8.3, 2.3, 1.9, 2]
         readings_TEMP = [22, 25, 97, 64, 24, 68, 99, 89, 22, 24]
         
         
         data.send(readings_TEMP, readings_PRESS)
-        data.update()
+        data.update(SM_dia, TH_dia)
     
-    innerfunc()
+    innerfunc(SM_dia, TH_dia)
 
 
 
@@ -382,11 +382,11 @@ class SendData:
         self.nodesHX[3].s = self.nodesSys[2].s + 0.03
             
         
-    def update(self):
+    def update(self, SM_dia, TH_dia):
 
-        
-        global SM_dia
-        global TH_dia
+#        
+#        global SM_dia
+#        global TH_dia
         
         SM_dia.update_data(self.nodesSys, self.nodesHX)
         TH_dia.update_data(self.nodesSys, self.nodesHX)
@@ -397,113 +397,10 @@ def timer(func, second=2, *arg):
     t = Timer(second, timer, args=(func, 3, *arg))
     t.setDaemon(True)
     
-    global on_click_loop
+    on_click_loop = arg[0]
     
     if t.daemon and on_click_loop:
         t.start()
     else:
 #        del readings_TEMP, readings_PRESS
         return 0
-
-            
-    
-        
-        
-if __name__=='__main__':
-    window = tk.Tk()
-    window.title("Lab429, ORC for 500W, author:wei")
-    w = tk.Label(window, text='this is ORC_GUI').pack()
-    
-    frame = tk.Frame(window).pack()
-    
-    
-    # left and right frame
-    frm_right = tk.Frame(frame)
-    frm_right.pack(side='right')
-#    tk.Label(frm_right, text='frame right').pack()
-
-    frm_left = tk.Frame(frame)
-    frm_left.pack(side='left')
-#    tk.Label(frm_left, text='frame left').pack()
-    
-    
-    # top and bottom of right frame
-    frm_right_top = tk.Frame(frm_right)
-    frm_right_top.pack(side='top')
-#    tk.Label(frm_right_top, text='frame right top').pack()
-    frm_right_bottom = tk.Frame(frm_right)
-    frm_right_bottom.pack(side='bottom')
-#    tk.Label(frm_right_bottom, text='frame right bottom').pack()
-    
-    
-    SM_dia = ORC_Status(frm_left)
-    TH_dia = ORC_Figure(frm_right_top)
-    
-    frm_right_bottom_left = tk.Frame(frm_right_bottom)
-    frm_right_bottom_left.pack(side='left')
-    frm_right_bottom_right = tk.Frame(frm_right_bottom)
-    frm_right_bottom_right.pack(side='right')    
-    
-    varScan = tk.StringVar()
-    labelScan = tk.Label(frm_right_bottom_left, textvariable=varScan, bg='white', \
-                 font=('Arial', 12), width=15, height=2)
-    labelScan.pack()
-
-    varmdotWater = tk.StringVar()
-    labelmdotWater = tk.Label(frm_right_bottom_right, textvariable=varmdotWater, bg='white', \
-                 font=('Arial', 12), width=15, height=2)
-    labelmdotWater.pack()
-    
-    on_click_loop = False
-    def btn_cmd_loop(func):
-        global on_click_loop
-        if on_click_loop == False:
-            on_click_loop = True
-            varScan.set('start2scan')
-            func()
-        else:
-            on_click_loop = False
-            varScan.set('stop2scan')
-
-            
-    def btn_cmd_one(func):
-        func()
-    
-    def good():
-        motWater = varmdotWater.get()
-#        print(motWater, type(motWater))
-        SM_dia.mdotWater = float(motWater)
-        labelmdotWater.config(text=str(motWater))
-        
-            
-    buttonScan = tk.Button(frm_right_bottom_left, text='click me', width=15, height=2, \
-                  command=lambda: btn_cmd_loop(test_scan_data))
-    buttonScan.pack()
-    
-    g = tk.Radiobutton(frm_right_bottom_right, text='熱水大流量',  variable=varmdotWater, value=0.29, \
-                  command=good)
-    g.pack()
-    gg = tk.Radiobutton(frm_right_bottom_right, text='熱水中流量', variable=varmdotWater, value=0.23, \
-                  command=good)
-    gg.pack()
-    ggg = tk.Radiobutton(frm_right_bottom_right, text='熱水小流量', variable=varmdotWater, value=0.17, \
-                  command=good)
-    ggg.pack()
-#    kkk = tk.Button(frm_right_bottom, text='scan', width=15, height=2, \
-#                  command=lambda: btn_cmd_loop(SM_dia.update_state))
-#    kkk.pack()
-#
-#    
-#    init_boundary = tk.Button(frm_right_bottom, text='init_boundary', width=15, height=2, \
-#                  command=lambda: btn_cmd_one(TH_dia.set_window_boundary))
-#    init_boundary.pack()
-    
-    
-    window.mainloop()
-
-    
-    
-    
-
-    
-
