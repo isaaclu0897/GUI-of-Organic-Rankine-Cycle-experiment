aa0f23eee2269247b7338a67e927916567b5aa7d
wei <t104306033@ntut.org.tw>
2021-07-26 01:38:59 +0800

    reset file structure

---
+++ __init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 26 01:35:22 2021
+
+@author: wei
+"""
---
+++ thermo/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 26 01:30:17 2021
+
+@author: wei
+"""
+print("You have imported mypackage")
